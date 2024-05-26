b454358e428c0f6ae9baadd36ccec108a1e72a99
NUKARAJU NERADABILLI <neradabillinukaraju@gmail.com>
2024-05-26 15:17:12 +0530

    commit

---
+++ Daily_Coding_In_Python/sumofcube.py
@@ -0,0 +1,5 @@
+n = int(input())
+res = 0
+for i in range(n):
+    res += (i*i*i)
+print(res)    
---
+++ Daily_Coding_In_Python/sumofnumbers.py
@@ -0,0 +1,5 @@
+n = int(input("Numbers: "))
+res = 0
+for i in range(n):
+  res += int(input())
+print(res)  
