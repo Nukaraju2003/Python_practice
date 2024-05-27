9f45f5fc1bd6ced2945a39d54bbee64ff69aa4f9
NUKARAJU NERADABILLI <neradabillinukaraju@gmail.com>
2024-05-27 18:00:25 +0530

    commit

---
+++ Daily_Coding_In_Python/krotation.py
@@ -0,0 +1,21 @@
+# a = [2, 4, 5,6, 7]
+# k = 3
+# for i  in range(0, k):
+#     temp = a[len(a)-1]
+#     for j in range(1, len(a)):
+#         if(j !=len(a)-1):
+#             a[j] = a[j-1]
+#     a[j] = temp
+#     print(a)    
+            
+
+
+# a = [1, 2, 3, 4, 5,6, 7]    
+# k = 3  
+# b = [3, 4,5]
+# print(a+b)
+
+# b = a[0: (len(a) -k)]
+# print(b)
+# c = a[len(a) -k ::]
+# print(c+b)
