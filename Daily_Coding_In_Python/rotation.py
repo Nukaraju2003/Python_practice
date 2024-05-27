674cfceae3e2ce320960fb6d649a2b9893952baf
NUKARAJU NERADABILLI <neradabillinukaraju@gmail.com>
2024-05-27 14:11:44 +0530

    commit

---
+++ Daily_Coding_In_Python/rotation.py
@@ -0,0 +1,40 @@
+# k = [1,2,4,3]
+# b= len(k)
+# for i in range(b):
+#     print(k[i], end = " ")
+    
+
+# # print(len(a))
+# # for i in range(len(a)):
+#     # print(a[i], end= "\n")
+
+# temp = k[0]
+# l = []
+# for j in range(b):        
+#     # print(a[j], end = " ")
+#     if(l[j] == b-1):
+#         l[j] = temp
+#     else:
+#         l[j] = l[j+1]
+#     print(l[j], end = " ") 
+    
+                
+# l = [45, 5, 6, 8]    
+# m = []
+# temp = l[0]
+# for i in range(len(l)):
+#     if(i == len(l)-1):
+#         m.append(temp)
+          
+#     else:
+#         m.append(l[i+1])
+# print(m) 
+        
+        
+#     m.append(l[i])
+# print(m)
+
+
+l = [45, 5, 6, 8]   
+temp = l[0]
+
