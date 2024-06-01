2ee945383f16e6d2957e8d7c3cd3e59740439e1d
NUKARAJU NERADABILLI <neradabillinukaraju@gmail.com>
2024-06-01 18:01:55 +0530

    commit

---
+++ Snakify/while/squares.py
@@ -0,0 +1,15 @@
+# i = 1
+# while i<= 10:
+#     print(i**2)
+#     i+=1
+
+
+i = 5
+while i >=1:
+    print("ram", end = " ")
+    j = 1
+    while j<=4:
+        print("rocks", end = " ")
+        j+=1
+    i -=1
+    print()
