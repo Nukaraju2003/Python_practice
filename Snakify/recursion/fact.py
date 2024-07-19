dfd1f4d72c62c992674b2152dda2d0e73a8e9098
NUKARAJU NERADABILLI <neradabillinukaraju@gmail.com>
2024-07-19 14:06:28 +0530

    commit

---
+++ Snakify/recursion/fact.py
@@ -0,0 +1,31 @@
+# factorial of a number
+# n = 5
+# res = 1
+# for i in range(1, n):
+#     res = res* i
+# print(res)
+
+
+# def factorial(n):
+#     res = 1
+#     for i in range(1, n+1):
+#         res = res* i
+#     return res
+
+# print(factorial(5))
+
+
+# def max(a, b):
+#     print("called")
+#     if (a> b):
+#         return a
+#     else:
+#         return b
+# print(max(5, 6))
+
+# print(max(int(input()), int(input())))
+
+
+# def maxi(*a):
+
+# print(maxi(5, 6, 7))
